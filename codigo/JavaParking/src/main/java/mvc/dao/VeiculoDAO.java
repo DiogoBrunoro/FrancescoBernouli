5a439145c6d290d4d434a9ac1ca4e87692be09c7
Diogo C Brunoro <diogocaribebrunoro@gmail.com>
2024-11-07 09:08:30 -0300

    FRANCESCO BRERNOULI

---
+++ codigo/JavaParking/src/main/java/mvc/dao/VeiculoDAO.java
@@ -0,0 +1,47 @@
+package mvc.dao;
+
+import java.io.Serializable;
+import java.util.List;
+
+import mvc.model.Veiculo;
+
+public class VeiculoDAO extends AbstractDAO<Veiculo> implements Serializable {
+
+    private static VeiculoDAO instance;
+
+    // Construtor privado para implementação do Singleton
+    private VeiculoDAO() {
+        super("turmamanha-g1-equipe-cao-do-xumelabs\\codigo\\JavaParking\\src\\main\\java\\mvc\\data\\Veiculos.dat");  // Define o caminho do arquivo .dat
+    }
+
+    // Método Singleton para garantir uma única instância
+    public static VeiculoDAO getInstance() {
+        if (instance == null) {
+            instance = new VeiculoDAO();
+        }
+        return instance;
+    }
+
+    public void cadastrarVeiculo(Veiculo veiculo) {
+        cadastrar(veiculo); 
+    }
+
+    public void removerVeiculo(Veiculo veiculo) {
+        remover(veiculo); 
+    }
+
+    public Veiculo pesquisarVeiculoPorPlaca(String placa) {
+        return listarTodos().stream()
+                .filter(veiculo -> veiculo.getPlaca().equals(placa))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public List<Veiculo> listarVeiculos() {
+        return listarTodos();  
+    }
+
+    public void atualizarVeiculo(Veiculo veiculoAntigo, Veiculo veiculoNovo) {
+        atualizar(veiculoAntigo, veiculoNovo); 
+    }
+}
